278645bc3712fa6b124636bb02e497b1216b531c
ushan senarathna <ushansenarathna000@gmail.com>
2024-12-16 15:19:05 +0530

    create and save

---
+++ src/main/java/org/example/springdatajpabasic/SpringDataJpaBasicApplication.java
@@ -0,0 +1,39 @@
+package org.example.springdatajpabasic;
+
+import org.example.springdatajpabasic.model.Student;
+import org.example.springdatajpabasic.repo.StudentRepo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDataJpaBasicApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(SpringDataJpaBasicApplication.class, args);
+
+        StudentRepo repo = context.getBean(StudentRepo.class);
+
+        Student s1 = context.getBean(Student.class);
+        Student s2 = context.getBean(Student.class);
+        Student s3 = context.getBean(Student.class);
+
+        s1.setRollNo(1);
+        s1.setName("ushan");
+        s1.setMarks(33);
+
+        s2.setRollNo(2);
+        s2.setName("kushan");
+        s2.setMarks(83);
+
+        s3.setRollNo(3);
+        s3.setName("thushan");
+        s3.setMarks(53);
+
+        repo.save(s1);
+        repo.save(s2);
+        repo.save(s3);
+    }
+
+}
---
+++ src/test/java/org/example/springdatajpabasic/SpringDataJpaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springdatajpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
